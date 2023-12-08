231da5fdcc5d0dceff83c5861d9380ac6b369511
hungnt <hungnt@regit-technology.com>
2023-12-08 14:42:56 +0700

    edit blog

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,9 +4,11 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Traits\BlogGate;
 
 class AuthServiceProvider extends ServiceProvider
 {
+    use BlogGate;
     /**
      * The model to policy mappings for the application.
      *
@@ -21,6 +23,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->blogGate();
     }
 }
